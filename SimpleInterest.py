f006bb652aef562cb0131530bad7db5ae514c1a0
Parth Sharma <parthsh.161106@gmail.com>
2026-09-21 17:21:14 +0530

    PythonPractice

---
+++ SimpleInterest.py
@@ -0,0 +1,9 @@
+P = int(input("Enter Principal Amount: "))
+R = int(input("Enter rate of interest: "))
+T = int(input("Enter time(in years): "))
+
+Product = P * R * T
+
+Simple_Interest = Product / 100
+
+print("Simple Interest is: ₹", Simple_Interest)
